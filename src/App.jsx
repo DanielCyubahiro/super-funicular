82c7ed02dcd9bef766ecce73f145dc81b27cdf3f
Daniel <cyubahirodaniel@gmail.com>
2025-04-02 15:30:36 +0200

    Add update feature feature

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,11 @@ function App() {
 
   const [colors, setColors] = useState(initialColors);
 
-  const handleAddNewColor = (newColor) => {
-    setColors([{id: uid(), ...newColor}, ...colors]);
+  const handleAddOrUpdateColor = (color) => {
+    'id' in color
+        ? setColors(colors.map(c => c.id === color.id ? color : c)) // Update color
+        : setColors([{id: uid(), ...color}, ...colors]); // New color
+
   };
 
   const handleDelete = (colorId) => {
@@ -19,13 +22,14 @@ function App() {
   return (
       <>
         <h1>Theme Creator</h1>
-        <ColorForm onAddNewColor={handleAddNewColor}/>
+        <ColorForm onAddNewColor={handleAddOrUpdateColor}/>
         {
           colors.length ? colors.map((color) => {
                 return <Color
                     key={color.id}
                     color={color}
                     onDelete={() => handleDelete(color.id)}
+                    onEdit={handleAddOrUpdateColor}
                 />;
               },
           ) : <p>No colors... start by adding one!</p>
