cf7a23dee5a4653bb9e63acd03f151c4e9747c0b
Daniel <cyubahirodaniel@gmail.com>
2025-04-02 16:23:59 +0200

    Add a timer and message after copying

--- src/Components/CopyToClipboard/CopyToClipboard.jsx
+++ src/Components/CopyToClipboard/CopyToClipboard.jsx
@@ -1,16 +1,34 @@
+import {useEffect, useState} from 'react';
+
 const CopyToClipboard = ({text}) => {
+  const [isCopied, setCopied] = useState(false);
+
   const writeClipboardText = async () => {
     try {
       await navigator.clipboard.writeText(text);
+      setCopied(true);
     } catch (error) {
       console.error(error.message);
     }
   };
+
+  useEffect(() => {
+    let timer;
+    if (isCopied) {
+      timer = setTimeout(() =>
+          setCopied(false), 3 * 1000,
+      );
+    }
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [isCopied]);
+
   return (
       <button
           onClick={writeClipboardText}
       >
-        Copy
+        {isCopied ? 'Successfully copied!' : 'Copy'}
       </button>
   );
 };
