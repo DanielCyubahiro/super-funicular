03aef742898d44ba9d3f1dc8db2a66853998d1c9
Daniel <cyubahirodaniel@gmail.com>
2025-04-02 14:26:42 +0200

    Add delete button and warning before deleting

--- src/Components/Color/Color.jsx
+++ src/Components/Color/Color.jsx
@@ -1,17 +1,33 @@
-import "./Color.css";
+import './Color.css';
+import {useState} from 'react';
 
-export default function Color({ color }) {
+export default function Color({color, onDelete}) {
+  const [showConfirm, setShowConfirm] = useState(false);
   return (
-    <div
-      className="color-card"
-      style={{
-        background: color.hex,
-        color: color.contrastText,
-      }}
-    >
-      <h3 className="color-card-headline">{color.hex}</h3>
-      <h4>{color.role}</h4>
-      <p>contrast: {color.contrastText}</p>
-    </div>
+      <div
+          className="color-card"
+          style={{
+            background: color.hex,
+            color: color.contrastText,
+          }}
+      >
+        <h3 className="color-card-headline">{color.hex}</h3>
+        <h4>{color.role}</h4>
+        <p>contrast: {color.contrastText}</p>
+        {
+            showConfirm && (
+                <>
+                  <h4 className="color-card-headline">really delete?</h4>
+                  <button onClick={() => setShowConfirm(false)}>
+                    Cancel
+                  </button>
+                </>
+            )
+        }
+        <button
+            onClick={showConfirm ? onDelete : () => setShowConfirm(true)}>
+          Delete
+        </button>
+      </div>
   );
 }
